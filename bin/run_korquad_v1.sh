9b028b53d87037fa251e2b2873845a3bf4400e62
uhmppi1 <uhmppi1@gmail.com>
2019-11-13 12:46:05 +0900

    shell modified korquadv1 sh added

--- bin/run_korquad_v1.sh
+++ bin/run_korquad_v1.sh
@@ -4,10 +4,9 @@ cd ..   # BASEDIR인 transformers 디렉토리에서 실행한다.
 BASEDIR=$(pwd)
 
 MODEL=bert
-#MODEL_NAME=bert-large-uncased-whole-word-masking
-MODEL_NAME=bert-base-uncased
-SQUAD_DIR=$BASEDIR/data/squad
-OUTPUT_DIR=$BASEDIR/models/wwm_uncased_finetuned_squad/
+MODEL_NAME=bert-base-multilingual-cased
+KORQUAD_DIR=$BASEDIR/data/korquad_v1
+OUTPUT_DIR=$BASEDIR/models/wwm_uncased_finetuned_korquad_v1/
 
 #python -m torch.distributed.launch --nproc_per_node=1 ./examples/run_squad.py \
 #python ./examples/run_squad.py \
@@ -18,8 +17,8 @@ python ./examples/run_squad.py \
     --do_train \
     --do_eval \
     --do_lower_case \
-    --train_file $SQUAD_DIR/train-v1.1.json \
-    --predict_file $SQUAD_DIR/dev-v1.1.json \
+    --train_file $KORQUAD_DIR/KorQuAD_v1.0_train.json \
+    --predict_file $KORQUAD_DIR/KorQuAD_v1.0_dev.json \
     --learning_rate 3e-5 \
     --num_train_epochs 2 \
     --max_seq_length 384 \
